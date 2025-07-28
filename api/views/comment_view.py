61a8b21250ed2ae693ca8748bcbc7a26ed90bb73
Rodrigo Oliveira <rodrigo.silva.oliveira369@gmail.com>
2025-07-27 21:44:23 -0300

    update CommentViewSet to use StandardCursorPagination and adjust permissions for list and retrieve actions

--- api/views/comment_view.py
+++ api/views/comment_view.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from api.authentication import FirebaseAuthentication
 from api.models import Comment, Post
+from api.pagination import StandardCursorPagination
 from api.permissions import IsAuthorOrReadOnly
 from api.serializers import CommentSerializer
 
@@ -9,7 +10,14 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     authentication_classes = [FirebaseAuthentication]
-    permission_classes = [IsAuthenticated & IsAuthorOrReadOnly]
+    pagination_class = StandardCursorPagination
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated & IsAuthorOrReadOnly]
+        return [permission() for permission in permission_classes]
 
     def perform_create(self, serializer):
         post = Post.objects.get(pk=self.kwargs['post_pk'])
